bffcffd40bb67550b19c3d75d33fbc3a9dfc163b
mrathoredevopsengineer <148685072+mrathoredevopsengineer@users.noreply.github.com>
2025-04-18 18:14:20 +0530

    remote commit

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "I am learnig git by using flask app"
+    return "We are all leaning git hub"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
