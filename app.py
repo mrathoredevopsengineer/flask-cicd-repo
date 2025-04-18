ade849efbe2b9ab623850eb2ca122abb2ade4bbe
mrathoredevopsengineer <148685072+mrathoredevopsengineer@users.noreply.github.com>
2025-04-18 18:18:35 +0530

    sikh lenge

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "We are all leaning git hub"
+    return "Sikhe lenge bhai esa kya"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
