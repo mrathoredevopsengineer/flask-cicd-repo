31a40b5b929c5d4b3f8761ebc81e95eb50eea94e
Mahesh <m.rathore176@gmail.com>
2025-04-18 12:28:01 +0000

    app-changed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask.Deploy via Docker"
+    return "I am learnig git by using flask app"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
